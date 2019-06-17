8ca89a8d7cb71e412b9412dd20035cd69daae496
Melanie Rogan <melanie.rogan@ft.com>
2019-06-17 10:00:45 +0100

    fixing tests

--- spec/peeps-view-spec.js
+++ spec/peeps-view-spec.js
@@ -4,7 +4,7 @@ describe('PeepsView', function() {
       var peepsView = new PeepsView();
       var peeps = [{"body": "test one"}, {"body": "test two"}];
       expect(peepsView.wrapInHTML(peeps)).toEqual(
-        "<ul><li><div>test one</div></li><li><div>test two</div></li></ul>"
+        "<p><p>test one</p><br><p>test two</p><br></p><br>"
       );
     });
   });
